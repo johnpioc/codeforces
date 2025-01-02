c550171f621a6941518084fff3fd78e652046936
John Pioc <johnpioc15@gmail.com>
2025-01-02 15:00:17 +1000

    concept implementation: monotonic increasing queue

---
+++ competitive-programming-theory/MonotonicIncreasingQueue.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MonotonicIncreasingQueue {
+    static Deque<Integer> increasingMonotonicQueue(int arr[], int n) {
+        Deque<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++) {
+            while (!queue.isEmpty() && queue.getLast() > arr[i]) {
+                queue.removeLast();
+            }
+
+            queue.addLast(arr[i]);
+        }
+
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 3, 8, 2, 6, 1, 9};
+        int n = arr.length;
+
+        Deque<Integer> queue = increasingMonotonicQueue(arr, n);
+
+        Iterator it = queue.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+    }
+}
